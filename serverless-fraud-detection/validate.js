d70069a265733faf35c0056aef35e404c7198bd6
Nazarii Cherkas <nazarii@hazelcast.com>
2019-04-19 16:54:37 +0300

    Switched to moment.js

--- serverless-fraud-detection/validate.js
+++ serverless-fraud-detection/validate.js
@@ -1,11 +1,13 @@
 const hazelcast = require('./hazelcast');
 const haversine = require('haversine');
+const moment = require('moment');
 
 exports.handle = async (request, context, callback) => {
     console.log('Got request: ' + JSON.stringify(request));
     context.callbackWaitsForEmptyEventLoop = false;
 
     let userId = request.userId;
+    let requestTimestampMillis = moment(request.transactionTimestamp).utc().valueOf();
 
     let hazelcastClient = await hazelcast.getClient();
     let airports = await hazelcastClient.getMap('airports');
@@ -19,7 +21,7 @@ exports.handle = async (request, context, callback) => {
         await users.set(userId, {
             userId: userId,
             lastCardUsePlace: request.airportCode,
-            lastCardUseTimestamp: request.transactionTimestamp
+            lastCardUseTimestamp: requestTimestampMillis
         });
         return callback(null, {valid: true, message: 'User data saved for future validations'});
     }
@@ -30,14 +32,13 @@ exports.handle = async (request, context, callback) => {
         return callback(null, {valid: true, message: 'Transaction performed from the same location'});
     }
 
-    // TODO: use moment.js
     let speed = getSpeed(lastAirport, user.lastCardUseTimestamp, nextAirport, request.transactionTimestamp);
     let valid = speed <= 13000; // 800 km/hr == ~13000 m/min
     let message = valid ? 'Transaction is OK' : 'Transaction is suspicious';
 
     // Update user data
     user.lastCardUsePlace = request.airportCode;
-    user.lastCardUseTimestamp = request.transactionTimestamp;
+    user.lastCardUseTimestamp = requestTimestampMillis;
     await users.set(userId, user);
 
     return callback(null, {valid: valid, message: message});
@@ -45,15 +46,9 @@ exports.handle = async (request, context, callback) => {
 
 let getSpeed = (lastAirport, lastUseTimestamp, nextAirport, requestTimestamp) => {
     // Time
-    let minutes = (requestTimestamp - lastUseTimestamp) / 60000;
+    let minutes = moment(requestTimestamp).diff(lastUseTimestamp, 'minutes');
     // Distance
     let meters = haversine(nextAirport, lastAirport, {unit: 'meter'});
     // Speed
     return meters / minutes;
 };
-
-// To run this locally:
-// exports.handle({userId: 102, airportCode: 'FRA', transactionTimestamp: 1552916100000}, {}, (error, result) => {
-//     console.log(error);
-//     console.log(result);
-// });
